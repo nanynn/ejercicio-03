623be8d3680eec5983292a44d78f97dcc2e7422b
Anayn Pavez <nanyn.pavez@gmail.com>
2017-04-24 13:23:26 -0300

    version inicial

---
+++ README.mg
@@ -0,0 +1,4 @@
+README.mg
+#Ejercicio 03
+*Realizar vinculacion de html con js
+*Convertidor de grados celsius a farenhait
---
+++ assets/js/javascript.js
@@ -0,0 +1,4 @@
+
+var arr = [];
+var farenhait = arr.map(function(num){return ((num * 9)/5) + 32;})
+console.log (farenhait);
